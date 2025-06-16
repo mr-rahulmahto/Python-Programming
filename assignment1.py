3a74098f583e00302a32c53881c49d45589c55cb
Rahul Mahto <rahulmahto2486@gmail.com>
2025-06-16 20:37:26 +0530

    Assignemnt 1

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+#Task 2
+a="Hello  , "
+x=input('Enter the First Name : ')
+y=input('Enter the Last Name :  ')
+b=" ! Welcome to the Python Program "
+print(a+x+y+b)
+
+#task 1
+x=int(input("Enter the first number :"))
+y=int(input("Enter the Second number :"))
+
+print("Addition of two Number :"  ,x+y)
+print("Subtract of two Number :"  ,x-y)
+print("Multiplication of two Number :"  ,x*y)
+print("Divide of two Number :"  ,x//y)1111
--- task2.py
+++
@@ -1,6 +0,0 @@
-#Task 2
-a="Hello  , "
-x=input('Enter the First Name : ')
-y=input('Enter the Last Name :  ')
-b=" ! Welcome to the Python Program "
-print(a+x+y+b)
